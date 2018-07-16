351d7aec6c75145475a323ac955236c812aca5a0
Alon Peleg <peleg619@gmail.com>
2018-07-16 03:13:06 +0300

    added modals for warnings

--- src/Components/AddFriendsPage.js
+++ src/Components/AddFriendsPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Image, Text, TouchableOpacity, Dimensions } from "react-native";
+import { View, Image, Text, TouchableOpacity, Dimensions,Alert,Modal } from "react-native";
 
 
 export default class AddFriendsPage extends Component {
@@ -7,7 +7,9 @@ export default class AddFriendsPage extends Component {
     super(props);
     this.state = {
       photos: [],
-      playerCounter: 0
+      playerCounter: 0,
+      modalVisible: false,
+      errMsg:''
     };
   }
 componentDidMount(){
@@ -74,24 +76,50 @@ componentDidMount(){
     let json = await res.json();
     let theJson = JSON.parse(json.d);
     this.setState({ playerCounter: theJson })
-  }
+   }
   checkPlayersFours=async()=>{
     await this.refreshPlayerCount();
-    if(this.state.playerCounter==4)
+    if(this.state.playerCounter>=4)
     {
-      this.goPlay();
+      if(this.state.playerCounter>4)
+      {
+        this.setState({ modalVisible: true, errMsg: "There Are Extra Players" })
+        this.timeOut = setTimeout(() => {
+          this.setState({ modalVisible: false })
+          this.goPlay();
+        }, 2500);
+      }
+      else {
+        this.goPlay();
+      }
+      
     }
     else {
-      console.warn("not enough players for 4");
+      this.setState({ modalVisible: true, errMsg: "Four Players Required" })
+      this.timeOut = setTimeout(() => {
+        this.setState({modalVisible:false})
+      }, 2500);
     }
   }
   checkPlayersTwos = async () => {
     await this.refreshPlayerCount();
-    if (this.state.playerCounter == 2) {
-      this.marioGame();
+    if (this.state.playerCounter >= 2) {
+      if (this.state.playerCounter > 2) {
+        this.setState({ modalVisible: true, errMsg: "There Are Extra Players" })
+        this.timeOut = setTimeout(() => {
+          this.setState({ modalVisible: false })
+          this.marioGame();
+        }, 2500);
+      }
+      else {
+        this.marioGame();
+      }
     }
     else {
-      console.warn("not enough players for 2");
+      this.setState({ modalVisible: true, errMsg: "Two Players Required" })
+      this.timeOut = setTimeout(() => {
+        this.setState({ modalVisible: false })
+      }, 2500);
     }
   }
   render() {
@@ -144,9 +172,33 @@ componentDidMount(){
             source={require("../images/refreshPlayers.png")}
           />
         </TouchableOpacity>
-        <View style={{ top: HEIGHT-450, left:WIDTHMIDDLE-60 }}>
+        <View style={{ top: HEIGHT-450, left:WIDTHMIDDLE-55 }}>
           <Text>Player Count: {this.state.playerCounter}</Text>
         </View>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={this.state.modalVisible}
+          onRequestClose={() => null}
+        >
+          <View
+            style={{
+              top: 160,
+              left: 2,
+              width: "100%",
+              height: "20%",
+              alignSelf: "center",
+              backgroundColor: "rgba(240, 240, 241,0.7)",
+              justifyContent:'center',
+              alignItems:'center'
+              
+            }}
+          >
+            <Text style={{ fontSize: 25, color: "rgb(163, 163, 194)", textAlign: 'center', }}>
+              {this.state.errMsg}
+              </Text>
+            </View>
+        </Modal>
       </View>
     );
   }
